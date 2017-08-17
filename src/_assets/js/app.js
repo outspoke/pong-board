68ff74974762044177519523487a237f142be8ec
Jay Nielsen <jayarnielsen@gmail.com>
2017-08-17 17:48:27 -0600

    first view built

--- src/_assets/js/app.js
+++ src/_assets/js/app.js
@@ -4,4 +4,24 @@ import 'foundation-sites';
 
 $(document).ready(function(){
   $(document).foundation();
+
+  var $carousel = $('.masonry-style-carousel');
+  var $slides = $carousel.find('.slide');
+  $slides.first().addClass('active');
+  $slides.eq(1).addClass('in');
+  setInterval(function(){
+    var activeIndex = $carousel.find('.active').index();
+    var nextIndex = activeIndex + 1;
+    if (nextIndex >= $slides.length) {
+      nextIndex = 0;
+    }
+    var upcomingIndex = nextIndex + 1;
+    if (upcomingIndex >= $slides.length) {
+      upcomingIndex = 0;
+    }
+    $slides.eq(activeIndex).addClass('out').removeClass('active');
+    $slides.eq(nextIndex).removeClass('in').addClass('active');
+    $slides.eq(upcomingIndex).addClass('in').removeClass('out');
+  }, 7000);
+
 });
